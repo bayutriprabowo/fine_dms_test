4bdc6845a4c70abdc90aada5d05502aeb67a46ab
Arthur Lapz <rlapz@gnuweeb.org>
2023-03-11 00:48:39 +0700

    Add jwt

--- utils/token.go
+++ utils/token.go
@@ -1,9 +1,47 @@
 package utils
 
-func GenerateToken(id int64) (string, error) {
-	return "", nil
+import (
+	"errors"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+func GenerateToken(secret []byte, id int64, exp time.Duration) (string, error) {
+	cl := jwt.MapClaims{
+		"user_id": id,
+		"exp":     time.Now().Add(exp).Unix(),
+	}
+
+	tok := jwt.NewWithClaims(jwt.SigningMethodHS256, cl)
+	sgTok, err := tok.SignedString(secret)
+	if err != nil {
+		log.Println("GenerateToken", err.Error())
+		return "", err
+	}
+
+	return sgTok, nil
 }
 
-func ValidateToken(tokStr string) (string, error) {
+func ValidateToken(tokStr string, secret []byte) (string, error) {
+	tok, err := jwt.Parse(tokStr, func(t *jwt.Token) (any, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected error")
+		}
+
+		return secret, nil
+	})
+
+	if err != nil {
+		log.Println("ValidateToken", err.Error())
+		return "", err
+	}
+
+	if cl, ok := tok.Claims.(jwt.MapClaims); ok && tok.Valid {
+		return fmt.Sprintf("%d", cl["user_id"]), nil
+	}
+
 	return "", nil
 }
