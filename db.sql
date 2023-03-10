e3b8535c761d42b506d0e34cb71a1fd424837e0c
Arthur Lapz <rlapz@gnuweeb.org>
2023-03-10 18:55:18 +0700

    db: Refator & simplify table schemes

--- db.sql
+++ db.sql
@@ -1,75 +1,52 @@
 -- table m_user
-create table m_user (
-    id bigserial primary key,
-    username varchar(64) not null unique,
-    password varchar(255) not null,
-    email varchar(128) not null unique,
-    first_name varchar(64) not null,
-    last_name varchar(64),
-    created_at timestamp not null,
-    updated_at timestamp not null
+CREATE TABLE m_user(
+	id		BIGSERIAL PRIMARY KEY,
+	username	VARCHAR(64) UNIQUE NOT NULL,
+	password	VARCHAR(255) NOT NULL,
+	email		VARCHAR(128) UNIQUE NOT NULL,
+	first_name	VARCHAR(64) NOT NULL,
+	last_name	VARCHAR(64),
+	created_at	TIMESTAMP NOT NULL,
+	updated_at	TIMESTAMP NOT NULL
 );
 
-
 -- table m_file
-create table m_file (
-    id bigserial primary key,
-    path varchar(255) not null,
-    ext varchar(64) not null,
-    user_id bigint not null,
-    created_at timestamp not null,
-    updated_at timestamp not null,
-    constraint fk_file_user
-		foreign key (user_id)
-			references m_user (id)
+CREATE TABLE m_file(
+	id		BIGSERIAL PRIMARY KEY,
+	path		VARCHAR(255) NOT NULL,
+	ext		VARCHAR(64) NOT NULL,
+	user_id		BIGINT REFERENCES m_user(id) NOT NULL,
+	created_at	TIMESTAMP NOT NULL,
+	updated_at	TIMESTAMP NOT NULL
 );
 
 -- table m_tags
-create table m_tags (
-    id bigserial primary key,
-    name varchar(64) not null,
-    created_at timestamp not null,
-    updated_at timestamp not null
+CREATE TABLE m_tags(
+	id		BIGSERIAL PRIMARY KEY,
+	name		VARCHAR(16) NOT NULL,
+	created_at	TIMESTAMP NOT NULL,
+	updated_at	TIMESTAMP NOT NULL
 );
 
 -- table t_tags
-create table t_tags (
-    id bigserial primary key,
-    tags_id bigint not null,
-    file_id bigint not null,
-
-    constraint fk_tags_tags
-		foreign key (tags_id)
-			references m_tags (id)
-    constraint fk_tags_file
-		foreign key (file_id)
-			references m_file (id)
+CREATE TABLE t_tags(
+	id		BIGSERIAL PRIMARY KEY,
+	tags_id		BIGINT REFERENCES m_tags(id) NOT NULL,
+	file_id		BIGINT REFERENCES m_file(id) NOT NULL
 );
 
--- table t_upload
-create table t_upload (
-    id bigserial primary key,
-    file_id bigint not null,
-    user_id bigint not null,
-    date timestamp not null,
-    constraint fk_upload_file
-		foreign key (file_id)
-			references m_file (id),
-	constraint fk_upload_user
-		foreign key (user_id)
-			references m_user (id)
+-- table t_file_upload
+CREATE TABLE t_file_upload(
+	id		BIGSERIAL PRIMARY KEY,
+	file_id		BIGINT REFERENCES m_file(id) NOT NULL,
+	user_id		BIGINT REFERENCES m_user(id) NOT NULL,
+	created_at	TIMESTAMP NOT NULL
 );
 
--- table t_download
-create table t_download (
-    id bigserial primary key,
-    file_id bigint not null,
-    user_id bigint not null,
-    date timestamp not null,
-    constraint fk_download_file
-		foreign key (file_id)
-			references m_file (id),
-	constraint fk_download_user
-		foreign key (user_id)
-			references m_user (id)
+-- table t_file_download
+CREATE TABLE t_file_download(
+	id		BIGSERIAL PRIMARY KEY,
+	file_id		BIGINT REFERENCES m_file(id) NOT NULL,
+	user_id		BIGINT REFERENCES m_user(id) NOT NULL,
+	created_at	TIMESTAMP NOT NULL
 );
