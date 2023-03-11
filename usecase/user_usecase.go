0796c3a2b0cd057ef46d1ce8b33ab22ac20d5b29
Arthur Lapz <rlapz@gnuweeb.org>
2023-03-11 10:53:48 +0700

    usecase: user: simplify user validations

--- usecase/user_usecase.go
+++ usecase/user_usecase.go
@@ -52,6 +52,39 @@ func (self *user) GetByUsername(uname string) (*model.User, error) {
 }
 
 func (self *user) Add(user *model.User) error {
+	if err := self.validateEmpty(user); err != nil {
+		return err
+	}
+
+	if err := self.validateDuplicate(user); err != nil {
+		return err
+	}
+
+	// TODO: hashing password
+
+	return self.userRepo.Create(user)
+}
+
+func (self *user) Edit(user *model.User) error {
+	if err := self.validateEmpty(user); err != nil {
+		return err
+	}
+
+	if err := self.validateDuplicate(user); err != nil {
+		return err
+	}
+
+	// TODO: hashing password
+
+	return self.userRepo.Update(user)
+}
+
+func (self *user) Del(id int) error {
+	return self.userRepo.Delete(id)
+}
+
+// private
+func (self *user) validateEmpty(user *model.User) error {
 	if len(user.Email) == 0 {
 		return ErrUsecaseEmptyEmail
 	}
@@ -64,6 +97,10 @@ func (self *user) Add(user *model.User) error {
 		return ErrUsecaseEmptyFname
 	}
 
+	return nil
+}
+
+func (self *user) validateDuplicate(user *model.User) error {
 	_, err := self.userRepo.SelectByUsername(user.Username)
 	if err != nil && err != repo.ErrRepoNoData {
 		return ErrUsecaseInternal
@@ -80,29 +117,5 @@ func (self *user) Add(user *model.User) error {
 		return ErrUsecaseExistsEmail
 	}
 
-	// TODO: hashing password
-
-	return self.userRepo.Create(user)
-}
-
-func (self *user) Edit(user *model.User) error {
-	if len(user.Email) == 0 {
-		return ErrUsecaseEmptyEmail
-	}
-
-	if len(user.Password) == 0 {
-		return ErrUsecaseEmptyPassword
-	}
-
-	if len(user.FirstName) == 0 {
-		return ErrUsecaseEmptyFname
-	}
-
-	// TODO: hashing password
-
-	return self.userRepo.Update(user)
-}
-
-func (self *user) Del(id int) error {
-	return self.userRepo.Delete(id)
+	return nil
 }
